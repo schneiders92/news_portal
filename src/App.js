5f657e35c6b00ca01c3955a2243576e98f15ee63
Schneider Sara <schneiders92@users.noreply.github.com>
2021-08-20 12:40:11 +0200

    app.js fetch working

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@
 import './App.css';
 
 function App() {
+  function getNews(){
+    fetch(`https://newsapi.org/v2/everything?q=apple&from=2021-08-19&to=2021-08-19&sortBy=popularity&apiKey=1ae7b13f9499425b8874d2b0123bcfaa`)
+        .then(resp=>resp.json())
+        .then(news=>console.log(news))
+  }
+
   return (
     <div>
-
+      <button onClick={getNews}>Get news</button>
     </div>
   );
 }
