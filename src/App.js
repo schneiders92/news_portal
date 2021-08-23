70a20e53f40c1fd49b4b6ed4c6c97dc7de164f42
Schneider Sara <schneiders92@users.noreply.github.com>
2021-08-23 13:51:50 +0200

    main page working, react-router dom installed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 
 import './App.css';
+import News from './News'
+import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom"
 
 function App() {
-  function getNews(){
-    fetch(`https://newsapi.org/v2/everything?q=apple&from=2021-08-19&to=2021-08-19&sortBy=popularity&apiKey=1ae7b13f9499425b8874d2b0123bcfaa`)
-        .then(resp=>resp.json())
-        .then(news=>console.log(news))
-  }
+
 
   return (
     <div>
-      <button onClick={getNews}>Get news</button>
+      <News></News>
     </div>
   );
 }
