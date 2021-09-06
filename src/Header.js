5a7ce65671aefceedb9efe8c61de2a7e30fefc9f
udvardiakos <82174805+udvardiakos@users.noreply.github.com>
2021-09-06 13:15:31 +0200

    wip: header , news css

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,67 @@
-import {Link}from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import "font-awesome/css/font-awesome.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import mglass from "./search.png";
+import bbclogo from "./BBC_logo.png";
 
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searched: "",
+    };
+  }
 
-export default function Header(){
-    return(
-        <header>
-            <Link to="/home">#home</Link>
-            <Link to="/politics">#politics</Link>
-            <Link to="/economy">#economy</Link>
-            <Link to="/sport">#sport</Link>
-            <Link to="/tech">#tech</Link>
-        </header>
-    )
+  render() {
+    return (
+      <div className="cont-div">
+        <div></div>
+        <div className="link-div">
+          <Link to="/home">
+            <img className="bbclogo" src={bbclogo} />
+          </Link>
+          <ul>
+            <div>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+            </div>
+            <div>
+              <li>
+                <Link to="/politics">Politics</Link>
+              </li>
+            </div>
+            <div>
+              <li>
+                <Link to="/economy">Economy</Link>
+              </li>
+            </div>
+            <div>
+              <li>
+                <Link to="/sport">Sport</Link>
+              </li>
+            </div>
+            <div>
+              <li>
+                <Link to="/tech">Tech</Link>
+              </li>
+            </div>
+          </ul>
+          <form className="search-form" action="/action_page.php">
+            <input
+              type="text"
+              value={this.state.searched}
+              placeholder="Search.."
+            />
+            <button type="submit" class="search-button">
+              <img src={mglass} />
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
--- src/News.js
+++ src/News.js
@@ -1,39 +1,40 @@
-import React, {useState, useEffect} from 'react'
-import "./News.css"
+import React, { useState, useEffect } from "react";
+import "./News.css";
 
 export default function News(props) {
+  let [news, setNews] = useState([]);
 
-    let [news, setNews] = useState([]);
+  useEffect(() => {
+    getNews();
+  }, []);
 
+  useEffect(() => {
+    getNews();
+  }, [props.category]);
 
-    useEffect(() => {
-        getNews()
-    }, []);
-
-    useEffect(()=>{
-        getNews()
-    }, [props.category])
-
-
-    function getNews() {
-        fetch(`https://newsapi.org/v2/everything?q=${props.category}&from=2021-08-26&to=2021-08-26&sortBy=popularity&apiKey=1ae7b13f9499425b8874d2b0123bcfaa`)
-            .then(resp => resp.json())
-            .then(news => setNews(news.articles))
-    }
-
-    return (
-            <div className="news-cont">
-                {news.map(article => {
-                    return (
-                        <div className="article-cont">
-                            <img src={article.urlToImage} alt=""/>
-                            <h1>{article.title}</h1>
-                            <h3>{article.author}</h3>
-                            <p>{article.description}</p>
-                            <a href={article.url}>Read more</a>
-                        </div>
-                    )
-                })}
-            </div>
+  function getNews() {
+    fetch(
+      `https://newsapi.org/v2/everything?q=${props.category}&from=2021-08-26&to=2021-08-26&sortBy=popularity&apiKey=1ae7b13f9499425b8874d2b0123bcfaa`
     )
+      .then((resp) => resp.json())
+      .then((news) => setNews(news.articles));
+  }
+
+  return (
+    <div className="news-cont">
+      {news.map((article) => {
+        return (
+          <div className="article-cont">
+            <img src={article.urlToImage} alt="" />
+            <h1>{article.title}</h1>
+            <div className="author-name-div">
+              <h3> {article.author}</h3>
+            </div>
+            {/* <p>{article.description}</p>
+                            <a href={article.url}>Read more</a> */}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
